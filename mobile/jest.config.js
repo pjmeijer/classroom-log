6e691d0f58e1d2911e573a4648d1f174178de81d
pjmeijer <pjmeijer@me.com>
2026-05-26 12:46:58 +0000

    feat(mobile): Expo bootstrap, theme tokens (variant C), date utils + tests

---
+++ mobile/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEach: [],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|expo-router|expo-sqlite|expo-audio))',
+  ],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**', '!**/babel.config.js', '!**/jest.config.js'],
+};
